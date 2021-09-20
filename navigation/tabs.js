41bdb1faed434b7bd42c4fb04e5e4294f50d1de5
Thomas Deaner <thde0834@colorado.edu>
2021-09-20 13:48:07 -0600

    Customized Bottom Tab Navigator

--- navigation/tabs.js
+++ navigation/tabs.js
@@ -1,18 +1,43 @@
 import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import HomeScreen from '../screens/HomeScreen';
-// import {SearchScreen} from '../screens/SearchScreen';
-// import {PostScreen} from '../screens/PostScreen';
-// import {ProfileScreen} from '../screens/ProfileScreen';
+// import SearchScreen from '../screens/SearchScreen';
+// import PostScreen from '../screens/PostScreen';
+// import ProfileScreen from '../screens/ProfileScreen';
 import SettingsScreen from '../screens/SettingsScreen';
+import Ionicons from '@expo/vector-icons/Ionicons';
+import { Colors } from 'react-native/Libraries/NewAppScreen';
 
 const Tab = createBottomTabNavigator();
 
-export default function Tabs(){
+const Tabs = () => {
     return (
-          <Tab.Navigator>
+        <Tab.Navigator
+            screenOptions={({ route }) => ({
+            tabBarIcon: ({ focused, color, size }) => {
+                let iconName;
+
+                if (route.name === 'Home') {
+                iconName = focused
+                    ? 'home-sharp'
+                    : 'home-outline';
+                } else if (route.name === 'Settings') {
+                iconName = focused 
+                    ? 'settings-sharp' 
+                    : 'settings-outline';
+                }
+                
+                // You can return any component that you like here!
+                return <Ionicons name={iconName} size={size} color={color} />;
+            },
+            tabBarActiveTintColor: 'darkviolet',
+            tabBarInactiveTintColor: 'gray',
+            })}
+        >
             <Tab.Screen name="Home" component={HomeScreen} />
             <Tab.Screen name="Settings" component={SettingsScreen} />
           </Tab.Navigator>
       );
 }
+
+export default Tabs;
